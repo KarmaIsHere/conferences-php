cbeed180d8d424a8ddcf590bf1c72abea0397636
KarmaIsHere <letrixswitch@gmail.com>
2023-04-24 00:22:30 +0300

    made login page look like the other pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,57 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://kit.fontawesome.com/ef958cf3d3.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://unpkg.com/nature-css@0.1.0/dist/nature.min.css">
     <title>Login Form</title>
-</head>
+    <link rel="stylesheet" href="https://unpkg.com/nature-css@0.1.0/dist/nature.min.css">
+    <style>
+        body {
+            background-image: url('https://media.giphy.com/media/l2v1YQMVH8rEAstMyK/giphy.gif');
+            background-size: cover;
+            background-position: center;
+            font-family: Trebuchet MS, sans-serif;
+            height: 100vh;
+            overflow: hidden;
+            color: black;
+        }
 
-            <h1 class="text-center mb-3"><i class="fas fa-sign-in-alt"></i>  Login</h1>
-<form action="{{route('login')}}" method="POST">
-    @csrf
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        form {
+            background-color: rgba(255, 255, 255, 0.7);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0,0,0,0.5);
+            width: 400px;
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        label, input, p {
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        input[type="submit"] {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <form action="{{route('login')}}" method="POST">
+        @csrf
+        <h1><i class="fas fa-sign-in-alt"></i>  Login</h1>
         <label for="username">Username</label>
         <input type="text" id="username" name="username" value="{{old('username')}}">
         @error('username')
@@ -24,11 +63,12 @@
         @error('password')
         <p>{{$message}}</p>
         @enderror
+
         <label for="remember-input">Remember me</label>
         <input type="checkbox" id="remember-input" name="remember" value="1" {{old('remember') ? 'checked' : ''}}>
-        <input type="submit" value="login" class="btn btn-primary">
-</form>
+
+        <input type="submit" value="Login" class="btn btn-primary">
+    </form>
+</div>
+</body>
 </html>
-
-
-
