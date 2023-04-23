d985a37f4e6679fbe815b6e60d03c424fd45242f
KarmaIsHere <letrixswitch@gmail.com>
2023-04-23 17:52:49 +0300

    Initial commit

---
+++ app/Http/Requests/StoreConferenceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreConferenceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+                'title'=>'required|min:3|max:30',
+                'content'=>'required|min:3|max:30',
+                'date' => '',
+                'address'=>'required|min:3|max:30'
+        ];
+    }
+}
