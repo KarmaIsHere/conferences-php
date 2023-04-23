1c0e45e600c17c4e1eea1c093e2e0534c752eb2d
KarmaIsHere <letrixswitch@gmail.com>
2023-04-24 00:12:08 +0300

    fixed alignment of creation form

--- resources/views/conferences/partials/form.blade.php
+++ resources/views/conferences/partials/form.blade.php
@@ -1,29 +1,32 @@
-<div>
-    <label for="title-input">Title: </label>
-    <input id="title-input" type="text" name="title" value="{{ old('title', optional($conference ?? null)->title) }}">
+<div class="form-group">
+    <label for="title-input">Title:</label>
+    <input id="title-input" type="text" name="title" value="{{ old('title', optional($conference ?? null)->title) }}" class="form-control">
     @error('title')
-    <p>{{ $message }}</p>
+    <p class="text-danger">{{ $message }}</p>
     @enderror
 </div>
-<div>
-    <label for="content-input">Content: </label>
-    <textarea id="content-input" type="text" name="content">{{ old('content', optional($conference ?? null)->content) }}</textarea>
+
+<div class="form-group">
+    <label for="content-input">Content:</label>
+    <textarea id="content-input" name="content" class="form-control">{{ old('content', optional($conference ?? null)->content) }}</textarea>
     @error('content')
-    <p>{{ $message }}</p>
+    <p class="text-danger">{{ $message }}</p>
     @enderror
 </div>
-<div>
-    <label for="date-input">Date: </label>
-    <input id="date-input" type="date" name="date" value="{{ old('date', optional($conference ?? null)->date) }}" required>
+
+<div class="form-group">
+    <label for="date-input">Date:</label>
+    <input id="date-input" type="date" name="date" value="{{ old('date', optional($conference ?? null)->date) }}" class="form-control" required>
     @error('date')
-    <p>{{ $message }}</p>
+    <p class="text-danger">{{ $message }}</p>
     @enderror
 </div>
-<div>
-    <label for="address-input">Address: </label>
-    <textarea id="address-input" type="text" name="address">{{ old('address', optional($conference ?? null)->address) }}</textarea>
+
+<div class="form-group">
+    <label for="address-input">Address:</label>
+    <textarea id="address-input" name="address" class="form-control">{{ old('address', optional($conference ?? null)->address) }}</textarea>
     @error('address')
-    <p>{{ $message }}</p>
+    <p class="text-danger">{{ $message }}</p>
     @enderror
 </div>
-<br>
+
